0beee3f9570df85b8bda0b70d1b67b2a392c7b9c
baba-beda <darya.nightmare@gmail.com>
2015-05-05 14:23:08 +0300

    wa3 in C

---
+++ src/matroids/A.java
@@ -0,0 +1,58 @@
+package matroids;
+
+import java.io.*;
+
+/**
+ * Created by daria on 05.05.15.
+ */
+public class A {
+    class FastScanner {
+        StreamTokenizer st;
+
+        FastScanner() {
+            st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        }
+
+        FastScanner(File f) {
+            try {
+                st = new StreamTokenizer(new BufferedReader(new FileReader(f)));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        int nextInt() throws IOException {
+            st.nextToken();
+            return (int) st.nval;
+        }
+
+        String nextString() throws IOException {
+            st.nextToken();
+            return st.sval;
+        }
+    }
+
+    FastScanner in;
+    PrintWriter out;
+
+    public void solve() throws IOException {
+
+    }
+
+    public void run() {
+        try {
+            in = new FastScanner(new File("shedule" + ".in"));
+            out = new PrintWriter("shedule" + ".out");
+
+            solve();
+
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new A().run();
+    }
+}
---
+++ src/matroids/C.java
@@ -0,0 +1,125 @@
+package matroids;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by daria on 05.05.15.
+ */
+public class C {
+    class FastScanner {
+        StreamTokenizer st;
+
+        FastScanner() {
+            st = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        }
+
+        FastScanner(File f) {
+            try {
+                st = new StreamTokenizer(new BufferedReader(new FileReader(f)));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        int nextInt() throws IOException {
+            st.nextToken();
+            return (int) st.nval;
+        }
+
+        String nextString() throws IOException {
+            st.nextToken();
+            return st.sval;
+        }
+    }
+
+    FastScanner in;
+    PrintWriter out;
+
+    ArrayList<Integer>[] graph;
+    boolean[] used;
+    int[] matching;
+    Pair[] order;
+
+    public void solve() throws IOException {
+        int n = in.nextInt();
+        used = new boolean[n];
+        order = new Pair[n];
+        graph = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+            order[i] = new Pair(i, in.nextInt());
+        }
+        Arrays.sort(order);
+
+        int secNumber = 0;
+
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+            int k = in.nextInt();
+            for (int j = 0; j < k; j++) {
+                int v = in.nextInt() - 1;
+                secNumber = Math.max(v + 1, secNumber);
+                graph[i].add(v);
+            }
+        }
+        
+        matching = new int[secNumber];
+        Arrays.fill(matching, -1);
+
+        for (int i = 0; i < n; i++) {
+            int v = order[i].v;
+            Arrays.fill(used, false);
+            kuhn(v);
+        }
+
+        for (int i = 0; i < secNumber; i++) {
+            out.print((matching[i] + 1) + " ");
+        }
+    }
+
+    boolean kuhn(int v) {
+        if (used[v]) {
+            return false;
+        }
+        used[v] = true;
+        for (int to : graph[v]) {
+            if (matching[to] == -1 || kuhn(matching[to])) {
+                matching[to] = v;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void run() {
+        try {
+            in = new FastScanner(new File("matching" + ".in"));
+            out = new PrintWriter("matching" + ".out");
+
+            solve();
+
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    class Pair implements Comparable<Pair>{
+        int v, weight;
+        public Pair(int v, int weight) {
+            this.v = v;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return Integer.compare(o.weight, this.weight);
+        }
+    }
+    public static void main(String[] args) {
+        new C().run();
+    }
+}
